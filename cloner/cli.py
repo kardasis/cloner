49bdebc5ee748f600000977cf695468fa9bb2036
Ari Kardasis <ari.kardasis@gmail.com>
2020-04-12 06:43:55 -0400

    detect environment variable and raise if missing

--- cloner/cli.py
+++ cloner/cli.py
@@ -1,10 +1,12 @@
 import click
+import os
 
 
 @click.command()
-def main():
-    print('hello world')
-    pass
-
-if __name__ == '__main__':
-    main()
+def main(git_user, git_repo):
+    cloner_path = os.getenv('CLONER_PATH')
+    if cloner_path is None:
+        msg = 'CLONER_PATH environment variable must be defined'
+        exc = click.ClickException(msg)
+        exc.exit_code = 1
+        raise exc
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,7 +3,8 @@ from click.testing import CliRunner
 from cloner import cli
 
 
-def test_cli():
+def test_cli(monkeypatch):
+    monkeypatch.delenv("CLONER_PATH", raising=False)
     runner = CliRunner()
-    result = runner.invoke(cli.command, ['not-a-valid-url'])
+    result = runner.invoke(cli.main)
     assert result.exit_code == 1
