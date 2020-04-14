e4a69a9974c92e22e8bb0e61ad42bd5b22e3f5e9
Ari Kardasis <ari.kardasis@gmail.com>
2020-04-13 20:58:01 -0400

    update setup.py to point to correct entry point and have correct meta-data

--- cloner/cli.py
+++ cloner/cli.py
@@ -11,16 +11,12 @@ class ClonerException(click.ClickException):
 
 @click.command()
 @click.argument('args', nargs=-1)
-def main(args):
+def cli(args):
     cloner_path = os.getenv('CLONER_PATH')
     if cloner_path is None:
         msg = 'CLONER_PATH environment variable must be defined'
-        raise click.ClonerException(msg)
-    cloner_path =  os.path.expanduser(cloner_path)
+        raise ClonerException(msg)
+    cloner_path = os.path.expanduser(cloner_path)
     ap = ArgParser(args)
     local_path = os.path.join(cloner_path, ap.local_path_tail)
     subprocess.call(["git", "clone", ap.url, local_path])
-
-
-if __name__ == '__main__':
-    main()
